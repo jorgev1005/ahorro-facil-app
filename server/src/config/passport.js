3a2a75a0a58ee68a6674efa5be8e177a5d1db979
Jorge Luis Verenzuela Morales <jorge.verenzuela@gmail.com>
2026-02-20 18:08:51 -0400

    Fix redirect_uri_mismatch by making callback URL absolute

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -5,7 +5,7 @@ const { User } = require('../models');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: "https://api.grupoaludra.com/api/auth/google/callback"
 },
     async function (accessToken, refreshToken, profile, cb) {
         try {
